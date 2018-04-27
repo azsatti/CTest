c8b56e2e597687457ce00c04e8208c5991cd3bea
aftab_zaheer <aftab.zaheer@share.co.uk>
2018-04-27 13:16:11 +0100

    moq tests

--- PairingTest.Unit.Tests/Web/QuestionnaireControllerTests.cs
+++ PairingTest.Unit.Tests/Web/QuestionnaireControllerTests.cs
@@ -1,6 +1,9 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using PairingTest.Web.Controllers;
+using PairingTest.Web.Interfaces;
 using PairingTest.Web.Models;
+using PairingTest.Web.Utility;
 
 namespace PairingTest.Unit.Tests.Web
 {
@@ -12,7 +15,10 @@ namespace PairingTest.Unit.Tests.Web
         {
             //Arrange
             var expectedTitle = "Geography Questions";
-            var questionnaireController = new QuestionnaireController();
+            var mockedApiSettings = new Mock<IApiSettings>();
+            mockedApiSettings.Setup(x => x.ApiUrl).Returns("http://localhost:50014/");
+            var mockedApiClient = new Mock<ApiClient>(mockedApiSettings.Object);
+            var questionnaireController = new QuestionnaireController(mockedApiClient.Object);
 
             //Act
             var result = (QuestionnaireViewModel)questionnaireController.Index().Result.ViewData.Model;
